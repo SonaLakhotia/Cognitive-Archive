a87ddf26885efaa3676fa41bd15a89179b31297c
SonaLakhotia <jss16sonal@gmail.com>
2024-06-27 19:47:48 +0200

    Data Lakes and Lakehouses

---
+++ Data Lakehouses-Developments in Data Warehoueses and Data Lakes/New-Lakehouse/configurations.py
@@ -0,0 +1,27 @@
+conf = (
+    pyspark.SparkConf()
+        .setAppName('app_name')
+        .set('spark.jars.packages', 'org.apache.iceberg:iceberg-spark-runtime-3.3_2.12:1.3.1,org.projectnessie.nessie-integrations:nessie-spark-extensions-3.3_2.12:0.67.0,software.amazon.awssdk:bundle:2.17.178,software.amazon.awssdk:url-connection-client:2.17.178')
+        .set('spark.sql.extensions', 'org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions,org.projectnessie.spark.extensions.NessieSparkSessionExtensions')
+        .set('spark.sql.catalog.nessie', 'org.apache.iceberg.spark.SparkCatalog')
+        .set('spark.sql.catalog.nessie.uri', NESSIE_URI)
+        .set('spark.sql.catalog.nessie.ref', 'main')
+        .set('spark.sql.catalog.nessie.authentication.type', 'NONE')
+        .set('spark.sql.catalog.nessie.catalog-impl', 'org.apache.iceberg.nessie.NessieCatalog')
+        .set('spark.sql.catalog.nessie.s3.endpoint', AWS_S3_ENDPOINT)
+        .set('spark.sql.catalog.nessie.warehouse', WAREHOUSE)
+        .set('spark.sql.catalog.nessie.io-impl', 'org.apache.iceberg.aws.s3.S3FileIO')
+        .set('spark.hadoop.fs.s3a.access.key', AWS_ACCESS_KEY)
+        .set('spark.hadoop.fs.s3a.secret.key', AWS_SECRET_KEY)
+)
+
+
+## Start Spark Session
+spark = SparkSession.builder.config(conf=conf).getOrCreate()
+print("Spark Running")
+
+
+## Test Run a Query
+spark.sql("CREATE TABLE IF NOT EXISTS nessie.test1 (name string) USING iceberg;").show()
+spark.sql("INSERT INTO nessie.test1 VALUES ('test');").show()
+spark.sql("SELECT * FROM nessie.test1;").show()
